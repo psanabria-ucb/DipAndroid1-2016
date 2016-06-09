f719e463227350edc2176d62eb7ec23dda33e748
Pablo Sanabria <psq1@ucbcba.edu.bo>
2016-06-08 22:08:36 -0700

    Clase 3 - Action Bar Menu

--- Activities/app/src/main/java/com/example/pablo/activities/DashboardActivity.java
+++ Activities/app/src/main/java/com/example/pablo/activities/DashboardActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class DashboardActivity extends AppCompatActivity {
@@ -12,6 +15,7 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
         SharedPreferences preferences = getSharedPreferences(
                 getString(R.string.app_name), MODE_PRIVATE);
         String username = preferences.getString("username", "");
@@ -22,5 +26,33 @@ public class DashboardActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Username: " + username, Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dashboard_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout_option:
+                logout();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        SharedPreferences preferences = getSharedPreferences(
+                getString(R.string.app_name), MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove("username");
+        editor.commit();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
