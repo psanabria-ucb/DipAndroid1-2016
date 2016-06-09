71b8d0eb0ba4aa059fe11984a12d7daccb55b8be
Pablo Sanabria <Pablo Sanabria>
2016-06-08 19:20:45 -0700

    Clase 1 && Clase 2

---
+++ Activities/app/src/main/java/com/example/pablo/activities/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.pablo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+    }
+
+    public void loginClick(View view) {
+        Log.d("MainActivity", "You clicked me!");
+        Log.d("MainActivity", "Username: " + username.getText());
+        Log.d("MainActivity", "Password: " + password.getText());
+        String rightUsername = "admin";
+        String rightPassword = "foobar";
+
+        if (rightUsername.equals(username.getText().toString()) &&
+                rightPassword.equals(password.getText().toString())) {
+            Toast.makeText(this, R.string.login_success, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, R.string.login_failed, Toast.LENGTH_LONG).show();
+        }
+    }
+}
