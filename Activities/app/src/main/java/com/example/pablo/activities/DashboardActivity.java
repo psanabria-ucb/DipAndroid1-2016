94ba987d9a6b7a9e4fd3aa88bead3c32c3e9bce6
Pablo Sanabria <psq1@ucbcba.edu.bo>
2016-06-08 21:36:27 -0700

    Clase 3, Con Login

--- Activities/app/src/main/java/com/example/pablo/activities/DashboardActivity.java
+++ Activities/app/src/main/java/com/example/pablo/activities/DashboardActivity.java
@@ -1,5 +1,7 @@
 package com.example.pablo.activities;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -10,9 +12,15 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        User user = (User)(getIntent().getSerializableExtra("USER"));
-        if (user != null) {
-            Toast.makeText(this, user.getUsername(), Toast.LENGTH_LONG).show();
+        SharedPreferences preferences = getSharedPreferences(
+                getString(R.string.app_name), MODE_PRIVATE);
+        String username = preferences.getString("username", "");
+        if (username.isEmpty()) {
+            // Usuario no logeado
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Username: " + username, Toast.LENGTH_LONG).show();
         }
     }
 }
--- Activities/app/src/main/java/com/example/pablo/activities/LoginActivity.java
+++ Activities/app/src/main/java/com/example/pablo/activities/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.pablo.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,7 +10,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     private EditText username;
     private EditText password;
 
@@ -21,23 +23,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loginClick(View view) {
-        Log.d("MainActivity", "You clicked me!");
-        Log.d("MainActivity", "Username: " + username.getText());
-        Log.d("MainActivity", "Password: " + password.getText());
+        Log.d("LoginActivity", "You clicked me!");
+        Log.d("LoginActivity", "Username: " + username.getText());
+        Log.d("LoginActivity", "Password: " + password.getText());
         String rightUsername = "admin";
         String rightPassword = "foobar";
 
         if (rightUsername.equals(username.getText().toString()) &&
                 rightPassword.equals(password.getText().toString())) {
             Toast.makeText(this, R.string.login_success, Toast.LENGTH_LONG).show();
-            User user = new User();
-
-            user.setUsername(username.getText().toString());
-            user.setPassword(password.getText().toString());
+            // SharedPreferences hace referencia a un archivo
+            // de configuracion
+            SharedPreferences preferences =
+                    getSharedPreferences(getString(R.string.app_name), // Nombre archivo
+                            Context.MODE_PRIVATE); // Solo nuestra app puede leer esto
+            SharedPreferences.Editor editor = preferences.edit(); // Abrir el archivo para escritura
+            editor.putString("username", username.getText().toString()); // Almacenar el valor
+            editor.commit(); // Guardar cambios
 
             Intent intent = new Intent(this, DashboardActivity.class);
-
-            intent.putExtra("USER", user);
             startActivity(intent);
         } else {
             Toast.makeText(this, R.string.login_failed, Toast.LENGTH_LONG).show();
