6a6f6a409cbd846340f95765826deb43c30a74c1
Pablo Sanabria <psq1@ucbcba.edu.bo>
2016-06-16 19:53:51 -0400

    Agregado tabbar

--- TabsExample/app/src/main/java/com/example/pablo/tabsexample/MainActivity.java
+++ TabsExample/app/src/main/java/com/example/pablo/tabsexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pablo.tabsexample;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -9,9 +10,18 @@ import com.example.pablo.tabsexample.model.Post;
 import com.example.pablo.tabsexample.network.RetrievePostsAsyncTask;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setElevation(0);
+        }
+        tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Posts"));
+        tabLayout.addTab(tabLayout.newTab().setText("Users"));
     }
 }
